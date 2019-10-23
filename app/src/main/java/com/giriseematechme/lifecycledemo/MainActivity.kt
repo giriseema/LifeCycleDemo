eb603a93e4589ce94c9bbbcaa63fa276058bbf97
techgiriseema <giriseematechme@gmail.com>
2019-10-23 14:17:36 +0530

    second commit

--- app/src/main/java/com/giriseematechme/lifecycledemo/MainActivity.kt
+++ app/src/main/java/com/giriseematechme/lifecycledemo/MainActivity.kt
@@ -2,11 +2,45 @@ package com.giriseematechme.lifecycledemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = this::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG,"Activity onResume method")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"Activity onStart method")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG,"Activity onRestart method")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"Activity onPause method")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"Activity onStop method")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"Activity onDestroy method")
     }
 }
